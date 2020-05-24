f553e8b6bd518671348a6a10f11376d600231ffb
Franklin Sales <franklin.salesx@gmail.com>
2020-05-24 19:13:03 -0300

    fix error on Header component

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+import Colors from '../constants/colors'
+
 const Header = props => {
     return (
-        <View style={styles.header}>
-            <Text style={styles.headerTitle}>{props.title}</Text>
-        </View>
+        <View style={styles.header}><Text style={styles.headerTitle}>{props.title}</Text></View>
     )
 }
 
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: '#f7287b',
+        backgroundColor: Colors.primary,
         alignItems: 'center',
         justifyContent: 'center'
     },
