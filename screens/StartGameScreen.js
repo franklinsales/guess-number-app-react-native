36ba8a388490b435e981b0391adcdefbb910d805
Franklin Sales <franklin.salesx@gmail.com>
2020-05-24 21:38:26 -0300

    implements reset input handler

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -13,6 +13,10 @@ const StartGameScreen = props => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''))
     }
 
+    const resetInputHandler = () => {
+        setEnteredValue('')
+    }
+
     return (
         <TouchableWithoutFeedback onPress={() => {
             Keyboard.dismiss()
@@ -34,7 +38,7 @@ const StartGameScreen = props => {
                     />
                     <View style={styles.buttonContainer}>
                         <View style={styles.button}>
-                            <Button title="Reset" style={styles.button} onPRess={() => {}} color={Colors.accent}/>
+                            <Button title="Reset" style={styles.button} onPress={resetInputHandler} color={Colors.accent}/>
                         </View>
                         
                         <View style={styles.button}>
