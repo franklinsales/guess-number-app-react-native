dda874844d4a26fdad355aaa548579862ac36ca6
Franklin Sales <franklin.salesx@gmail.com>
2020-05-29 22:52:17 -0300

    implements result container

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, Image, ImageBackground} from 'react-nat
 
 import BodyText from '../components/BodyText'
 import TitleText from '../components/TitleText'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 const GameOverScreen = props => {
     return (
@@ -17,8 +18,14 @@ const GameOverScreen = props => {
                     resizeMode="cover"
                 />
             </View>
-            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-            <BodyText>Number was: {props.userNumber}</BodyText>
+
+            <View style={styles.resultContainer}>
+                <BodyText style={styles.resultText}>
+                    Your phone needed:
+                    <Text style={styles.highlight}> {props.roundsNumber} </Text> rounds to guess the number:
+                    <Text> {props.userNumber} </Text>
+                </BodyText>
+            </View>
             <Button title="New Games!" onPress={props.onRestart}/>
         </View>
     )
@@ -42,6 +49,18 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: '100%'        
+    },
+    resultContainer: {
+        marginHorizontal: 40,
+        marginVertical: 25
+    },
+    resultText:{
+        textAlign: "center",
+        fontSize: 20
+    },
+    highlight:{
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold'
     }
 })
 
