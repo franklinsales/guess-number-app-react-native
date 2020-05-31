4089cf3437ffd36945ac280f6d6e539624c0bfbc
Franklin Sales <franklin.salesx@gmail.com>
2020-05-31 00:09:09 -0300

    style list of past guesses

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,6 +6,7 @@ import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card'
 import DefaultStyles from '../constants/default-styles'
 import MainButton from '../components/MainButton'
+import BodyText from '../components/BodyText'
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min)
@@ -17,6 +18,13 @@ const generateRandomBetween = (min, max, exclude) => {
         return rndNum
     }
 }
+
+const renderListItem = (value, numOfRound) => (
+    <View key={value} style={styles.listItem}>
+        <BodyText>#{numOfRound}</BodyText>
+        <BodyText>{value}</BodyText>
+    </View>
+);
 
 const GameScreen = props => {
     const initialGuess = generateRandomBetween(1, 100, props.userChoice)
@@ -54,7 +62,7 @@ const GameScreen = props => {
         //setRounds(curRounds => curRounds + 1)
         setPastGuesses(curPastGuesses => [nextNumber, ...curPastGuesses])
     }
-
+    
     return (
         <View style={styles.screen}>
             <Text style={DefaultStyles.title}>Opponent's Guess</Text>
@@ -67,13 +75,11 @@ const GameScreen = props => {
                     <Ionicons name="md-add" size={24} color="white"/>
                 </MainButton>
             </Card>
-            <ScrollView>
-                {pastGuesses.map(guess => (
-                    <View key={guess}>
-                        <Text>{guess}</Text>
-                    </View>
-                ))}
-            </ScrollView>
+            <View style={styles.list}>
+                <ScrollView>
+                    {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length-index))}
+                </ScrollView>
+            </View>
         </View>
     )
 }
@@ -90,6 +96,19 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: 320,
         maxWidth: '90%'
+    },
+    list:{
+        width: '80%',
+        flex: 1
+    },
+    listItem: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-between'    
     }
 })
 
